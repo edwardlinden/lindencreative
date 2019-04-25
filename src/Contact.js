ec19aeee40486a4e0c6cd6aef757ae072ff96042
edwardlinden <edward.linden@live.se>
2019-04-25 23:30:37 +0200

    innan deploy

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,6 @@ export const Contact = () => (
     <div>
         <h4>Contact</h4>
         <p>Connect with us if you want to challenge us with a case.</p>
-        <p>Do you think you have what it takes to join our tribe? Give us a pling!!1</p>
+        <p>Do you think you have what it takes to join our tribe? Give us a pling!</p>
     </div>
 )
